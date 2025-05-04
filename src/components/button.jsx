e58c8b047a8602bb868a53df32a0ff8e4a332cad
Latipzhan uulu Adilet <the65462@gmail.com>
2025-05-04 20:05:03 +0600

    new Button Style

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -6,10 +6,10 @@ export default function Button() {
   return (
     <div>
       <StyleDiv>
-        <ButtonStyle title="outlined" variant="outlined" />
-        <ButtonStyle title="contained" variant="contained" />
-        <ButtonStyle title="Warning" variant="Warning" />
-        <ButtonStyle title="Danger" variant="Danger" />
+        <ButtonStyle title="outlined" variant="outlined" disabled/>
+        <ButtonStyle title="contained" variant="contained" disabled/>
+        <ButtonStyle title="Warning" variant="Warning" disabled/>
+        <ButtonStyle title="Danger" variant="Danger" disabled/>
       </StyleDiv>
     </div>
   );
